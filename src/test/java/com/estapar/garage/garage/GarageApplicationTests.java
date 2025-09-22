9733d7561b2b095b053827de1c0f7b4a68b7736f
Felipe Carvalho <felipe.carvalho@postalis.com.br>
2025-09-22 00:20:19 -0300

    [TASK] excluir GarageApplicationTests do ciclo de testes Maven

--- src/test/java/com/estapar/garage/garage/GarageApplicationTests.java
+++ src/test/java/com/estapar/garage/garage/GarageApplicationTests.java
@@ -1,13 +1,13 @@
 package com.estapar.garage.garage;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Ignorando contextLoads no CI")
 class GarageApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
